bb789016846edcb70b69139313435138f3d39aa6
Kostiantyn Rakilov <rakilovkg@gmail.com>
2025-08-23 22:47:01 +0300

    Visual fix of 'Start Scan' button

--- src/NewScanModal.jsx
+++ src/NewScanModal.jsx
@@ -533,7 +533,7 @@ const NewScanModal = ({ isOpen, onClose }) => {
           {scanType === 'Deals' && <DealsForm />}
           <button
             type="submit"
-            className="w-full py-2 bg-green-600 text-white rounded hover:bg-green-700 hover:cursor-pointer transition"
+            className="w-full py-2 bg-green-600 text-white rounded hover:bg-green-700 hover:cursor-pointer transition disabled:bg-gray-400"
             disabled={submitRequest.loading}
           >
             Start Scan
