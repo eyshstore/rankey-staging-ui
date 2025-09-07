ea6e0a18fb7b0dc50f92c5c2bd9648f001b7ebc1
Kostiantyn Rakilov <rakilovkg@gmail.com>
2025-09-07 22:38:21 +0300

    Updated environment variable reading

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
-const isProduction = process.env.NODE_ENV === 'production';
-
 const config = {
-  apiBaseUrl: isProduction ? process.env.REACT_APP_API_BASE_URL : 'http://localhost:7000',
+  apiBaseUrl: import.meta.env.VITE_API_BASE_URL
 };
 
 export default config;
